e1db3af1904633439f6f4eed69ba070b89f63edc
jackorn ( JIGAR SONI ) <51534139+jigarsoni17@users.noreply.github.com>
2022-11-03 15:05:21 +0530

    Update nohup.sh

--- nohup.sh
+++ nohup.sh
@@ -7,19 +7,12 @@ ip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
 
 for i in $(find /home/*/*/nohup.out -type f -size +3G )
 do
-    if [[ -z "$i" ]]    
-    then
-        echo "Zaveri" 
-    else
 
-        name=$(find /home/*/*/nohup.out -type f -size +3G | head -1 | awk -F / '{ print $4; }')
-        mv $i $i-$date-$name;         
-
-    fi 
-done
+    name=$(find /home/*/*/nohup.out -type f -size +3G | head -1 | awk -F / '{ print $4; }')
+    mv $i $i-$date-$name;
+    c=$( echo $i | cut -d/ -f1-4 ) 
+    sh $c/start.sh
+done 
 
 mv $(find /home/*/*/nohup.out* -type f -size +3G) ~/nohupfiles/
-
-sh service restart command
-
-aws s3 cp ~/nohupfiles/* s3://admin-panel-logs/$ip/
+aws s3 cp ~/nohupfiles/*  s3://admin-panel-logs/$ip/
